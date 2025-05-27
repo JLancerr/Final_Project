d19862250997857906a664c45ed2224b7c21bb2d
Lance Padillo <lancejaypadillo@gmail.com>
2025-05-27 16:16:07 +0800

    Updated SQL file

--- misc/topup_db.sql
+++ misc/topup_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2025 at 10:27 AM
+-- Generation Time: May 27, 2025 at 10:15 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,16 +24,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `games`
+--
+
+CREATE TABLE `games` (
+  `game_id` int(11) NOT NULL,
+  `game_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `games`
+--
+
+INSERT INTO `games` (`game_id`, `game_name`) VALUES
+(100000, 'Genshin Impact'),
+(100001, 'Mobile Legends');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `transactions`
 --
 
 CREATE TABLE `transactions` (
   `transaction_id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `game_type` varchar(50) DEFAULT NULL,
-  `account_id` varchar(50) DEFAULT NULL,
+  `game_id` int(11) DEFAULT NULL,
+  `account_id` varchar(50) DEFAULT NULL COMMENT 'In-game ID',
+  `amount` int(11) DEFAULT NULL,
+  `payment_type` varchar(50) DEFAULT NULL,
   `purchase_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`transaction_id`, `user_id`, `game_id`, `account_id`, `amount`, `payment_type`, `purchase_date`) VALUES
+(1, 100000, 100000, '123', 60, NULL, NULL),
+(2, 100000, 100000, '123', 60, NULL, NULL),
+(3, 100000, 100000, '123', 60, NULL, NULL),
+(4, 100000, 100000, '123', 60, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -48,8 +79,30 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `password`) VALUES
+(100000, 'test.@gmail.com', 'AyakaLaosNa'),
+(100001, 'test.@gmail.com', 'AyakaLaosNa'),
+(100002, 'test.@gmail.com', 'AyakaLaosNa'),
+(100003, 'test.@gmail.com', 'AyakaLaosNa'),
+(100004, 'test.@gmail.com', 'AyakaLaosNa'),
+(100005, 'test.@gmail.com', 'AyakaLaosNa'),
+(100006, 'test.@gmail.com', 'AyakaLaosNa'),
+(100007, 'test.@gmail.com', 'AyakaLaosNa'),
+(100008, 'test.@gmail.com', 'AyakaLaosNa'),
+(100009, 'test.@gmail.com', 'AyakaLaosNa');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`game_id`);
 
 --
 -- Indexes for table `transactions`
@@ -68,16 +121,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `games`
+--
+ALTER TABLE `games`
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100002;
+
+--
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100000;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100010;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
