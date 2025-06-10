e72ca5ba80a18afe56a9754022185cc3fb0b7a42
Lance Padillo <lancejaypadillo@gmail.com>
2025-06-10 22:33:24 +0800

    Working Login and Signup. Commiting kasi magaadd ng looping sa dashboard.

--- php/login.php
+++ php/login.php
@@ -1,41 +1,41 @@
 <?php
     include('../php/connect.php');
 
-    if (!$_POST['email'] || !$_POST['password']) {
-        header('Location: ../templates/landing.html?error=missing_inputs');
+    if (!$_POST['username'] || !$_POST['password']) {
+        header('Location: ../Frontend/Login/Login.html?error=missing_inputs');
         exit(); 
     }
 
     $admin_email = "admin@gmail.com";
     $admin_password = "Ayucka";
-    if ($_POST['email'] == $admin_email) {
+    if ($_POST['username'] == $admin_email) {
         if ($_POST['password'] == $admin_password) {
-            header('Location: ../templates/admin.php');
+            header('Location: ../templates/admin.php'); # Remember to change this
             exit();
         }
     }
 
     $conn->begin_transaction();
     try {
-        $query = $conn->prepare('SELECT * FROM users WHERE email = ?');
-        $query->bind_param('s', $_POST['email']);
+        $query = $conn->prepare('SELECT * FROM users WHERE username = ?');
+        $query->bind_param('s', $_POST['username']);
         $query->execute();
         $user_info = $query->get_result()->fetch_assoc();
         $conn->commit();
     }
     catch (mysqli_sql_exception $exception)  {
         $conn->rollback();
-        header('Location: ../templates/landing.html?error=login_failed');
+        header('Location: ../Frontend/Login/Login.html?error=login_failed');
         exit();
     }
 
     if (empty($user_info)) {
-        header('Location: ../templates/landing.html?error=login_failed');
+        header('Location: ../Frontend/Login/Login.html?error=login_failed');
         exit();
     }
 
     if (!password_verify($_POST['password'], $user_info['password'])) {
-        header('Location: ../templates/landing.html?error=login_failed');
+        header('Location: ../Frontend/Login/Login.html?error=login_failed');
         exit();
     }
 
@@ -44,7 +44,7 @@
     $_SESSION['email'] = $user_info['email'];
     $_SESSION['name'] = $user_info['name'];
     
-    header('Location: ../templates/home.php');
+    header('Location: ../Frontend/Dashboard/Dashboard.php');
     exit();
 
 ?>
--- php/signup.php
+++ php/signup.php
@@ -1,40 +1,51 @@
 <?php
     include('../php/connect.php');
 
-    if (!$_POST['email'] || !$_POST['password'] || !$_POST['name']) {
-        header('Location: ../templates/landing.html?error=missing_inputs');
+    if (!$_POST['email'] || !$_POST['password'] || !$_POST['full_name'] || !$_POST['username'] || !$_POST['number'] || !$_POST['birthdate']) {
+        header('Location: ../Frontend/Register/Register.html?error=missing_inputs');
         exit(); 
     }
 
-    if (strlen($_POST['email']) > 100 || strlen($_POST['password']) > 70 || strlen($_POST['name']) > 50) {
-        header('Location: ../templates/landing.html?error=length_exceeded');
+    if (strlen($_POST['email']) > 100 || strlen($_POST['password']) > 70 || strlen($_POST['full_name']) > 50 || strlen($_POST['username']) > 50) {
+        header('Location: ../Frontend/Register/Register.html?error=length_exceeded');
         exit(); 
     }
 
     $conn->begin_transaction();
     try {
+        # Check for duplicate emails
         $query = $conn->prepare("SELECT user_id FROM users WHERE email = ?");
         $query->bind_param("s", $_POST['email']);
         $query->execute();
         $result = $query->get_result();
         $duplicates = $result->fetch_all();
         if (count($duplicates) > 0) {
-            header('Location: ../templates/landing.html?error=duplicate_email');
+            header('Location: ../Frontend/Register/Register.html?error=duplicate_email');
+            exit();
+        }
+        # Check for duplicate usernames
+        $query = $conn->prepare("SELECT user_id FROM users WHERE username = ?");
+        $query->bind_param("s", $_POST['username']);
+        $query->execute();
+        $result = $query->get_result();
+        $duplicates = $result->fetch_all();
+        if (count($duplicates) > 0) {
+            header('Location: ../Frontend/Register/Register.html?error=duplicate_username');
             exit();
         }
 
         $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        $query = $conn->prepare('INSERT INTO users (email, password, name) VALUES (?, ?, ?)');
-        $query->bind_param('sss', $_POST['email'], $hashed_password, $_POST['name']);
+        $query = $conn->prepare('INSERT INTO users (email, password, full_name, username, number, birthdate) VALUES (?, ?, ?, ?, ?, ?)');
+        $query->bind_param('ssssis', $_POST['email'], $hashed_password, $_POST['full_name'], $_POST['username'], $_POST['number'], $_POST['birthdate']);
         $query->execute();
 
         $conn->commit();
-        header('Location: ../templates/landing.html?status=signup_success');
+        header('Location: ../Frontend/Register/Register.html?status=signup_success');
         exit();
     }
     catch (mysqli_sql_exception $exception)  {
         $conn->rollback();
-        header('Location: ../templates/landing.html?error=signup_failed');
+        header('Location: ../Frontend/Register/Register.html?error=signup_failed');
         exit();
     }
 ?>
